8ea792d21c81a20f966a758f572bb881441c6164
isle-prashant <isle.prashant@hotmail.com>
2016-04-25 22:49:59 +0530

    socket io implemented

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,4 @@
-/*var express = require('express');
-var http = require('http').Server(express);
-var io = require('socket.io')(http);
-var router = express.Router();
-
-/!* GET home page. *!/
-router.get('/', function(req, res, next) {
- // res.render('index', { title: 'Express' });
-  path = 'index.html';
-    res.sendfile('index.html');
-});
-
-
-io.on('connection', function(socket){
-    console.log("User got connected");
-});
-
-module.exports = router;*/
-module.exports = function(io) {
+module.exports = function (io) {
 
     var express = require('express');
     var router = express.Router();
@@ -29,11 +11,11 @@ module.exports = function(io) {
     });
 
 
-    io.on('connection', function(socket){
-        socket.on('chat message', function(msg){
+    io.on('connection', function (socket) {
+        socket.on('chat message', function (msg) {
             io.emit('chat message', msg);
         });
     });
 
-     return router;
+    return router;
 }
